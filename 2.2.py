b51eeeec35e8a05d94e8b3bbfcc4d249015956dd
Tmaz5307 <144672191+Tmaz5307@users.noreply.github.com>
2023-10-18 19:46:28 +0900

    2.2

---
+++ 2.2.py
@@ -0,0 +1,8 @@
+string_a = input()
+string_b = input()
+if string_a == "ДА" and string_b == "ДА":
+    print("ВЕРНО")
+elif string_a == "НЕТ" and string_b == "НЕТ":
+    print("ВЕРНо")
+else:
+    print("НЕВЕРНО")
