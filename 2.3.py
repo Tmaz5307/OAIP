2e6891164c7e106de7c60e6517ba833bd54cd680
Tmaz5307 <144672191+Tmaz5307@users.noreply.github.com>
2023-10-18 19:54:15 +0900

    2.3

---
+++ 2.3.py
@@ -0,0 +1,8 @@
+string_a = input()
+string_b = input()
+string_c = input()
+if string_a == "раз" or string_a == "1" and string_b == "два" or string_b == "2"\
+        and string_c == "три" or string_c == "3":
+    print("ГОРИ")
+else:
+    print("НЕ ГОРИ")
