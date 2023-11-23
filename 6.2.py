940588ae9852a66d30a9e5351072f444d54c2c73
Tmaz5307 <144672191+Tmaz5307@users.noreply.github.com>
2023-11-23 13:12:21 +0900

    6.2.py

--- 6.2.py
+++ 6.2.py
@@ -1,13 +1,9 @@
-def sortirovka(a):
-    for i in range(1, len(a)):
-        b = a[i]
-        j = i - 1
-        while j >= 0 and a[j] > b:
-            a[j + 1] = a[j]
-            j -= 1
-        a[j + 1] = b
-
-
-c = [4, 2, 1, 6, 3, 5]
-sortirovka(c)
-print(c)
+a = [4, 2, 1, 6, 3, 5]
+for i in range(1, len(a)):
+    b = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > b:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = b
+print(a)
