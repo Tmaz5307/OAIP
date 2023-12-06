035420037cfa58b97bb199c8b8958ea6c3032fe5
Tmaz5307 <144672191+Tmaz5307@users.noreply.github.com>
2023-12-06 11:53:20 +0900

    9.4

---
+++ 9.4.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        a = int(input())
+        b = int(input())
+        c = a + b
+    except ValueError:
+        print("Введите только целочисленные значения")
+    else:
+        print(c)
+        break
