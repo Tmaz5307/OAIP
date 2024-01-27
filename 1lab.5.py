fd477b662d7836211772e6507c25f16586b7e27d
Tmaz5307 <144672191+Tmaz5307@users.noreply.github.com>
2024-01-27 20:13:51 +0900

    1lab.5.py

--- 1lab.5.py
+++ 1lab.5.py
@@ -1,13 +1,13 @@
-a = open("input.txt", encoding="utf-8")
+a = open("lines.txt", encoding="utf-8")
 b = 0
 for line in a:
     b += len(line)
     c = b * 8
     d = b
     e = b / 1000
-    f = e / 1000
-    g = f / 1000
-    h = g / 1000
+    f = b / 10**6
+    g = b / 10**9
+    h = b / 10**12
 print(c, "бит")
 print(d, "байт")
 print(e, "мегабайт")
