c388f7ecbbd6d3a3a93c27413f03be36af9ee181
Tmaz5307 <144672191+Tmaz5307@users.noreply.github.com>
2024-05-16 08:50:27 +0900

    Lab_11

---
+++ Web.py
@@ -0,0 +1,22 @@
+class Widget:
+    pass
+
+
+class Label:
+    pass
+
+
+class LineEdit(Widget):
+    pass
+
+
+class TextEdit(Widget):
+    pass
+
+
+class Button(Label):
+    pass
+
+
+class CheckBox(Label):
+    pass
